9b53eb2af74fcf879017dad3eeddf6f4e8f461ff
esnya <2088693+esnya@users.noreply.github.com>
2025-05-30 02:57:50 +0900

    chore(encoding): :recycle: convert BOM files to UTF-8 and add .gitattributes

--- ViveStreamingFaceTrackingDriver.cs
+++ ViveStreamingFaceTrackingDriver.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Elements.Core;
 using FrooxEngine;
 using ResoniteModLoader;
--- ViveStreamingFaceTrackingModule/ViveStreamingFaceTrackingInterface.cs
+++ ViveStreamingFaceTrackingModule/ViveStreamingFaceTrackingInterface.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
